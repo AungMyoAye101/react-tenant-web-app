55093fbc564783a9e51544070fbdb09bcc6af184
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-10-25 22:47:38 +0630

    feat : Add colors and typo and navbar.

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
-const navbar = () => {
+import logo from "@/assets/icons/logo-1.svg"
+import { Link, useLocation } from "@tanstack/react-router"
+import { Button } from "../ui/button"
+const Navbar = () => {
+    const { pathname } = useLocation()
+    console.log(pathname)
+    if (pathname === '/auth/login' || pathname === "/auth/signup") {
+        return;
+    }
     return (
-        <div>navbar</div>
+        <nav className="flex justify-between items-center gap-4 py-2 border-b">
+            <Link to={'/'} >
+                <div className="flex items-center gap-2">
+                    <img src={logo} alt="Next flow logo" className="size-10" />
+                    <h4 className="font-semibold">Next Flow</h4>
+                </div>
+            </Link>
+            {/* Navigtion */}
+            <div className="flex items-center gap-2">
+                <Link to={'/auth/sigup'}>
+                    <Button> Sigup</Button>
+                </Link>
+                <Link to={'/auth/login'}>
+                    <Button >Login</Button>
+                </Link>
+            </div>
+        </nav>
     )
 }
 
-export default navbar
+export default Navbar
--- src/features/auth/login/index.tsx
+++ src/features/auth/login/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import image from '@/assets/images/hero-illu.svg'
 import logo from '@/assets/icons/logo-1.svg'
 import LoginForm from '../components/loginForm'
