bdedf7aa2871da2753618761c6e3d3df0d887b70
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-10-31 23:50:31 +0630

    feat : add loading and not found components in customer service.

---
+++ src/features/customer-service/components/NotFoundService.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundService = () => {
+    return (
+        <div>NotFoundService</div>
+    )
+}
+
+export default NotFoundService
---
+++ src/features/customer-service/components/ServiceCardLoading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ServiceCardLoading = () => {
+    return (
+        <div>ServiceCardLoading</div>
+    )
+}
+
+export default ServiceCardLoading
